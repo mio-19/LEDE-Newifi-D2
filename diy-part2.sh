0e6c9b8f24b3220454d2da8f5ad22c0a6c298a25
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-08-01 19:01:22 +0800

    add luci-app-ttyd

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,3 +27,5 @@ git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome packag
 echo 'CONFIG_PACKAGE_luci-app-adguardhome=y' >> .config
 
 echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-ttyd=y' >> .config
