e348b83a13c032c4885b939d1f0789a520379ce3
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-08-01 18:44:48 +0800

    add luci-app-adguardhome and luci-app-aria2

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,3 +22,8 @@ echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> .config
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner package/lean/
 echo 'CONFIG_PACKAGE_luci-app-nlbwmon=y' >> .config
 echo 'CONFIG_PACKAGE_luci-app-onliner=y' >> .config
+
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/lean/
+echo 'CONFIG_PACKAGE_luci-app-adguardhome=y' >> .config
+
+echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> .config
