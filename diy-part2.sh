2be8c7df8acf40d50e7cb6b7424c0222a224cd53
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-08-01 22:29:38 +0800

    diy

--- diy-part2.sh
+++ diy-part2.sh
@@ -18,14 +18,14 @@ git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lea
 echo 'CONFIG_PACKAGE_luci-theme-argon=y' >> .config
 sed -i '/^CONFIG_PACKAGE_luci-theme-bootstrap=y$/d' .config
 
-#git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/lean/
+#git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/lean/OpenClash
 #echo 'CONFIG_PACKAGE_luci-app-openclash=y' >> .config
 
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner package/lean/
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner package/lean/luci-app-onliner
 #echo 'CONFIG_PACKAGE_luci-app-nlbwmon=y' >> .config # already in .config
 echo 'CONFIG_PACKAGE_luci-app-onliner=y' >> .config
 
-git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/lean/
+git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome package/lean/luci-app-adguardhome
 echo 'CONFIG_PACKAGE_luci-app-adguardhome=y' >> .config
 
 echo 'CONFIG_PACKAGE_luci-app-aria2=y' >> .config
