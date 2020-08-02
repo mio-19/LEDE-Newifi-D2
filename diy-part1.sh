a0e03263d7901f7c0be533f0f819808fd5d1e5e9
ㄗㄠˋ ㄑㄧˊ <tsao-chi@the-lingo.org>
2020-08-02 15:43:46 +0800

    add https://github.com/openwrt/luci.git

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,8 @@ set -e
 # Uncomment a feed source
 sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+# Add feed sources
 sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git openwrtluci https://github.com/openwrt/luci.git' feeds.conf.default
